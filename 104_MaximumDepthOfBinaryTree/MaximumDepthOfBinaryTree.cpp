135f42ebe42255047b495b52d613a367c1673834
jsambhav5 <jsambhav5@gmail.com>
2024-08-26 13:05:45 +0530

    Depth of Binary Tree

---
+++ 104_MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class BinaryTree {
+public:
+	TreeNode* treeBuilder() {
+		int val;
+		cin >> val;
+		if (!val) return NULL;
+		TreeNode* root = new TreeNode(val);
+		root->left = treeBuilder();
+		root->right = treeBuilder();
+		return root;
+	}
+
+	int maxDepth(TreeNode* root) {
+		if (!root) return 0;
+		int leftHeight = maxDepth(root->left);
+		int rightHeight = maxDepth(root->right);
+		return max(leftHeight, rightHeight) + 1;
+	}
+};
+
+int main(int argc, const char** argv) {
+	BinaryTree t1;
+	cout << t1.maxDepth(t1.treeBuilder());
+	return 0;
+}
--- 4_MedianOfTwoSortedArray/MedianOfSortedArrays.cpp
+++ 4_MedianOfTwoSortedArray/MedianOfSortedArrays.cpp
@@ -1,10 +1,10 @@
 // Array Merge
 /*
-1, 3, 8 = 3
+	1, 3, 8 = 3
 
-2, 4, 6, 12, 18 = 9
+	2, 4, 6, 12, 18 = 9
 
-1, 2, 3, 4, 6, 8, 12, 18 = 5
+	1, 2, 3, 4, 6, 8, 12, 18 = 5
 */
 
 
@@ -18,8 +18,9 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 	double result;
 	int i = 0, j = 0;
 	while (i < nums1.size() && j < nums2.size()) {
-		if (nums1[i] <= nums2[j])
+		if (nums1[i] <= nums2[j]) {
 			arr.push_back(nums1[i++]);
+		}
 		else
 			arr.push_back(nums2[j++]);
 	}
@@ -39,8 +40,21 @@ double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 }
 
 int main(int argc, const char** argv) {
-	// vector<int> nums1 = { 1, 3, 8 }, nums2 = { 2, 4, 6, 12, 18 };
-	vector<int> nums1 = { 1, 2 }, nums2 = { 3, 4 };
+	int size1, size2, input;
+	cin >> size1 >> size2;
+
+	vector<int> nums1, nums2;
+
+	for (int i = 0; i < size1; i++) {
+		cin >> input;
+		nums1.push_back(input);
+	}
+
+	for (int i = 0; i < size2; i++) {
+		cin >> input;
+		nums2.push_back(input);
+	}
+
 	cout << findMedianSortedArrays(nums1, nums2);
 	return 0;
 }
