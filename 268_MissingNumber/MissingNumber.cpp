64abf996dfd0397e99e8c7b0f84ad0363e7584ac
jsambhav5 <jsambhav5@gmail.com>
2024-10-04 21:47:24 +0530

    268

--- 268_MissingNumber/MissingNumber.cpp
+++ 268_MissingNumber/MissingNumber.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
 	int missingNumber(vector<int>& nums) {
-		// int sum = 0;
-		// for(int i = 0; i < n; i++) {
-		//     sum += nums[i];
-		// }
-		// return ((n * (n + 1)) / 2) - sum ;
 		int n = nums.size();
 		int xor_ = 0;
 		for (int i = 0; i < n; i++) {
 			xor_ = xor_ ^ nums[i] ^ i;
 		}
 		return xor_ ^ n;
+		// int sum = 0;
+		// for(int i = 0; i < n; i++) {
+		//     sum += nums[i];
+		// }
+		// return ((n * (n + 1)) / 2) - sum ;
 	}
 };
