348fcab2d674fbdd1077c9955aa586d2b36c09fd
jsambhav5 <jsambhav5@gmail.com>
2024-10-06 22:50:39 +0530

    26

--- 26_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.cpp
+++ 26_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.cpp
@@ -1,6 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
 	int removeDuplicates(vector<int>& nums) {
@@ -15,13 +12,3 @@ public:
 		return slow + 1;
 	}
 };
-
-int main(int argc, const char** argv) {
-	Solution s;
-	vector<int> nums = {1,1,2,2,2,3,3 };
-	int k = s.removeDuplicates(nums);
-	cout << k << endl;
-	for(int i : nums)
-		cout << i << " ";
-	return 0;
-}
