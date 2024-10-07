c77f883d2d2c9b66ae7553de24045b0b1c842c22
jsambhav5 <jsambhav5@gmail.com>
2024-10-08 01:08:51 +0530

    507

--- 507_PerfectNumber.cpp/PerfectNumber.cpp
+++ 507_PerfectNumber.cpp/PerfectNumber.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
 	bool checkPerfectNumber(int num) {
+		if (num <= 0) return false;
 		int sum = 0;
-		for (int i = 1; i <= num / 2; i++)
-			if (num % i == 0)
+		for (int i = 1; i * i <= num; i++) {
+			if (num % i == 0) {
 				sum += i;
-		return sum == num;
+				if (i != num / i)
+					sum += num / i;
+			}
+		}
+		return sum - num == num;
 	}
 };
