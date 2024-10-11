b587847420c6d25f6222fd7441fee83269fa572b
jsambhav5 <jsambhav5@gmail.com>
2024-10-11 18:23:49 +0530

    21

--- 21_MergeTwoSortedLists/MergeTwoSortedLists.cpp
+++ 21_MergeTwoSortedLists/MergeTwoSortedLists.cpp
@@ -15,30 +15,21 @@ public:
 	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
 		if (list1 == NULL) return list2;
 		if (list2 == NULL) return list1;
-		ListNode* itr;
-		if (list1->val < list2->val) {
-			itr = list1;
-			list1 = list1->next;
-		}
-		else {
-			itr = list2;
-			list2 = list2->next;
-		}
+		ListNode* itr = new ListNode();
 		ListNode* head = itr;
 		while (list1 != nullptr && list2 != nullptr) {
 			if (list1->val < list2->val) {
 				itr->next = list1;
-				itr = itr->next;
 				list1 = list1->next;
 			}
 			else {
 				itr->next = list2;
-				itr = itr->next;
 				list2 = list2->next;
 			}
+			itr = itr->next;
 		}
 		if (list1 == nullptr) itr->next = list2;
-		else if (list2 == nullptr) itr->next = list1;
-		return head;
+		else itr->next = list1;
+		return head->next;
 	}
 };
