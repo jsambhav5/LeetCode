ced28098a8736d71b69ea21dc17ba266cd054fbc
jsambhav5 <jsambhav5@gmail.com>
2024-10-13 17:50:18 +0530

    169

---
+++ 169_MajorityElement/MajorityElement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		int count = 0, candidate;
+		for (int i : nums) {
+			if (count == 0)
+				candidate = i;
+			if (i == candidate)
+				count++;
+			else
+				count--;
+		}
+		return candidate;
+	}
+};
