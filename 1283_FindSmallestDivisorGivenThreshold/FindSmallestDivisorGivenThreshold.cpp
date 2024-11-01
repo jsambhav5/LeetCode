b1df5930a89298bd856330ef5fd52431c95ecd1b
jsambhav5 <jsambhav5@gmail.com>
2024-11-01 19:33:46 +0530

    1283

--- 1283_FindSmallestDivisorGivenThreshold/FindSmallestDivisorGivenThreshold.cpp
+++ 1283_FindSmallestDivisorGivenThreshold/FindSmallestDivisorGivenThreshold.cpp
@@ -1,28 +1,35 @@
 class Solution {
 private:
-	int sumByD(vector<int>& nums, int threshold) {
-		int n = nums.size();
+	bool possible(vector<int>& nums, int threshold, int div) {
 		int sum = 0;
+		int n = nums.size();
+
 		for (int i = 0; i < n; i++) {
-			sum += ceil((double)(nums[i]) / (double)(threshold));
+			sum += nums[i] / div;
+
+			if (nums[i] % div != 0) sum++;
 		}
-		return sum;
+
+		return sum <= threshold;
 	}
 
 public:
 	int smallestDivisor(vector<int>& nums, int threshold) {
-		int n = nums.size();
-		if (n > threshold) return -1;
-		int low = 1, high = *max_element(nums.begin(), nums.end());
+		int low = 1, high = INT_MIN;
+		int ans = -1;
+		for (int i : nums)
+			high = max(high, i);
+
 		while (low <= high) {
 			int mid = (low + high) / 2;
-			if (sumByD(nums, mid) <= threshold) {
+
+			if (possible(nums, threshold, mid)) {
+				ans = mid;
 				high = mid - 1;
 			}
-			else {
-				low = mid + 1;
-			}
+			else low = mid + 1;
 		}
-		return low;
+
+		return ans;
 	}
 };
