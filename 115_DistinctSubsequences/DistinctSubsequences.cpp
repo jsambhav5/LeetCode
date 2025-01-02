3013408b35161717fc74ad46c490494c9515549d
Sambhav Jain <jsambhav5@gmail.com>
2025-01-02 09:58:14 +0530

    Create DistinctSubsequences.cpp

---
+++ 115_DistinctSubsequences/DistinctSubsequences.cpp
@@ -0,0 +1,20 @@
+class Solution{
+public:	
+    const int prime = 1e9 + 7;
+    int numDistinct(string s, string t){
+        int n = s.size();
+        int m = t.size();
+		vector<int> prev(m + 1, 0);
+		prev[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j >= 1; j--) { 
+                if (s[i - 1] == t[j - 1]) {
+                    prev[j] = (prev[j - 1] + prev[j]) % prime;
+                }
+            }
+        }
+
+        return prev[m];
+	}
+};
