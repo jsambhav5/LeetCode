1b6fc3516a8cbddbe66d8ba133486e2b9a6913cb
Sambhav Jain <jsambhav5@gmail.com>
2025-01-09 06:52:34 +0530

    Create DeleteOperationForTwoStrings.cpp

---
+++ 583_DeleteOperationForTwoStrings/DeleteOperationForTwoStrings.cpp
@@ -0,0 +1,26 @@
+class Solution{
+private:
+    int lcs(string s1, string s2) {
+        int n = s1.size();
+        int m = s2.size();
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1[ind1 - 1] == s2[ind2 - 1])
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                else
+                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]);
+            }
+            prev = cur;
+        }
+        return prev[m];
+    }
+public:
+    int minDistance(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        int k = lcs(str1, str2);
+        return (n - k) + (m - k);
+    }
+};
