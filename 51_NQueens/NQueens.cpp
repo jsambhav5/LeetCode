8392abb593ca31d20ba2806448d4b5cf03cf4c2a
Sambhav Jain <jsambhav5@gmail.com>
2025-01-23 21:48:03 +0530

    Create NQueens.cpp

---
+++ 51_NQueens/NQueens.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool safe(vector<string>& board, int row, int col) {
+        int r = row, c = col;
+
+        while (r >= 0 && c >= 0) {
+            if (board[r][c] == 'Q') return false;
+            r--;
+            c--;
+        }
+
+        r = row;
+        c = col;
+
+        while (c >= 0) {
+            if (board[r][c] == 'Q') return false;
+            c--;
+        }
+
+        r = row;
+        c = col;
+
+        while (r < board.size() && c >= 0) {
+            if (board[r][c] == 'Q') return false;
+            r++;
+            c--;
+        }
+        return true;
+    }
+
+    void func(int col, vector<vector<string>>& ans, vector<string>& board) {
+        if (col == board.size()) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int row = 0; row < board.size(); row++) {
+            if (safe(board, row, col)) {
+                board[row][col] = 'Q';
+                func(col + 1, ans, board);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+        func(0, ans, board);
+        return ans;
+    }
+};
