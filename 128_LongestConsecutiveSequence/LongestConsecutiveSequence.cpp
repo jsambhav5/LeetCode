3837b2f6a6c63b33f9872f634b8dd7f21defac2d
jsambhav5 <jsambhav5@gmail.com>
2025-02-07 08:36:48 +0530

    Longest Consecutive Sequence

--- 128_LongestConsecutiveSequence/LongestConsecutiveSequence.cpp
+++ 128_LongestConsecutiveSequence/LongestConsecutiveSequence.cpp
@@ -1,3 +1,12 @@
+// LeetCode : 128 - Longest Consecutive Sequence (https://leetcode.com/problems/longest-consecutive-sequence/description/)
+// TUF+ : Longest Consecutive Sequence in an Array (https://takeuforward.org/plus/dsa/hashing/faqs/longest-consecutive-sequence-in-an-array)
+
+/*
+Used hashing to solve this
+
+TC: O(N ^ 2)
+SC: O(N)
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -34,11 +43,47 @@ public:
 };
 
 int main() {
-	vector<int> a = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
+	int t;
+	cin >> t;
 
-	Solution solution;
+	while (t--) {
+		int n;
+		cin >> n;
 
-	int ans = solution.longestConsecutive(a);
-	cout << ans;
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			int input;
+			cin >> input;
+			arr.push_back(input);
+		}
+
+		Solution sol;
+
+		int res = sol.longestConsecutive(arr);
+
+		cout << res << endl;
+	}
+
 	return 0;
 }
+
+/*
+Test Input :
+3
+
+6
+100 4 200 1 3 2
+
+10
+0 3 7 2 5 8 4 6 0 1
+
+7
+1 9 3 10 4 20 2
+
+Test Output : 1
+4
+9
+4
+
+*/
