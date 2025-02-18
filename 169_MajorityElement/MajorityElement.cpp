061e7e63c634fddac33a8ec3d6fbb424594fd3f5
jsambhav5 <jsambhav5@gmail.com>
2025-02-18 21:57:03 +0530

    Majority Element

--- 169_MajorityElement/MajorityElement.cpp
+++ 169_MajorityElement/MajorityElement.cpp
@@ -1,3 +1,19 @@
+// LeetCode : 169 - Majority Element (https://leetcode.com/problems/majority-element/description/)
+// TUF+ : Majority Element-I (https://takeuforward.org/plus/dsa/arrays/faqs-hard/majority-element-i)
+
+/*
+Approach:
+Used Moore's Voting Algorithm to solve this
+
+TC: O(N)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Moore's Voting Algorithm
+
 class Solution {
 public:
 	int majorityElement(vector<int>& nums) {
@@ -13,3 +29,37 @@ public:
 		return candidate;
 	}
 };
+
+int main(int argc, const char** argv) {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			int input;
+			cin >> input;
+			arr.push_back(input);
+		}
+
+		Solution sol;
+
+		int res = sol.majorityElement(arr);
+		cout << res << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input :
+INPUT
+
+Test Output : 1
+OUTPUT
+
+*/
