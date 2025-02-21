d500e9af85c22f7c9dc20463dfcf0d848b8a3aa6
jsambhav5 <jsambhav5@gmail.com>
2025-02-21 14:53:25 +0530

    Search Insert Position

--- 35_SearchInsertPosition/SearchInsertPosition.cpp
+++ 35_SearchInsertPosition/SearchInsertPosition.cpp
@@ -1,3 +1,20 @@
+// LeetCode : 35 - Search Insert Position (https://leetcode.com/problems/search-insert-position/)
+// TUF+ : Search Insert Position (https://takeuforward.org/plus/dsa/binary-search/logic-building/search-insert-position)
+
+/*
+Topics and Learnings: Binary Search
+
+Approach:
+Used Binary Search to solve this
+Same as "Lower Bound"
+
+TC: O(logN)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
 	int searchInsert(vector<int>& nums, int target) {
@@ -18,3 +35,65 @@ public:
 		return ans;
 	}
 };
+
+int main(int argc, const char** argv) {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			int input;
+			cin >> input;
+			arr.push_back(input);
+		}
+
+		int target;
+		cin >> target;
+
+		Solution sol;
+
+		int ans = sol.searchInsert(arr, target);
+
+		cout << ans << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input :
+5
+
+6
+-1 0 3 5 9 12
+9
+
+6
+-1 0 3 5 9 12
+2
+
+4
+1 2 2 3
+2
+
+5
+3 5 8 15 19
+9
+
+5
+3 5 8 15 19
+3
+
+Test Output : 1
+4
+2
+1
+3
+0
+
+*/
