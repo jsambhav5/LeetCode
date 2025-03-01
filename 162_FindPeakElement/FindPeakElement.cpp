1a618f8e770913822050bd294e5b0f4ef6f8963f
jsambhav5 <jsambhav5@gmail.com>
2025-03-01 13:17:43 +0530

    Find Peak Element

--- 162_FindPeakElement/FindPeakElement.cpp
+++ 162_FindPeakElement/FindPeakElement.cpp
@@ -1,26 +1,79 @@
+// LeetCode : 162 - Find Peak Element (https://leetcode.com/problems/find-peak-element/description/)
+// TUF+ : Find peak element (https://takeuforward.org/plus/dsa/binary-search/faqs/find-peak-element)
+
+/*
+Topics and Learnings: Binary Search
+
+Approach:
+Used Binary Search to solve this
+
+Complexiy Analysis:
+TC: O(logN)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    int findPeakElement(vector<int> &arr) {
-        int n = arr.size();
+	int findPeakElement(vector<int>& nums) {
+		int n = nums.size();
 
-        if(n == 1) return 0;
-        if (arr[0] > arr[1]) return 0;
-        if(arr[n - 1] > arr[n - 2]) return n - 1;
-        
-        int low = 1, high = n - 2;
+		if (n == 1) return 0;
+		if (nums[0] > nums[1]) return 0;
+		if (nums[n - 1] > nums[n - 2]) return n - 1;
 
-        while(low <= high) {
-            int mid = (low + high) / 2;
-            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
-                return mid;
+		for (int i = 1; i < n - 1; i++)
+			if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
+				return i;
 
-            if(arr[mid] > arr [mid - 1])
-                low = mid + 1;
-
-            else
-                high = mid - 1;
-        }
-
-        return -1;
-    }
+		return n - 1;
+	}
 };
+
+int main(int argc, const char** argv) {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			int input;
+			cin >> input;
+			arr.push_back(input);
+		}
+
+		Solution sol;
+
+		int res = sol.findPeakElement(arr);
+
+		cout << res << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input :
+3
+
+10
+1 2 3 4 5 6 7 8 5 1
+
+7
+1 2 1 3 4 6 4
+
+5
+-2 -1 3 4 5
+
+Test Output : 1
+7
+1
+4
+
+*/
