6a077dcd84c91b7436df68f12729a22a4e0a7f05
jsambhav5 <jsambhav5@gmail.com>
2025-03-02 10:15:39 +0530

    Find a Peak Element II

--- 1901_FindAPeakElementII/FindPeakElement2.cpp
+++ 1901_FindAPeakElementII/FindPeakElement2.cpp
@@ -1,3 +1,20 @@
+// LeetCode : 1901 - Find a Peak Element II (https://leetcode.com/problems/find-a-peak-element-ii/description/)
+// TUF+ : Find Peak Element (https://takeuforward.org/plus/dsa/binary-search/2d-arrays/find-peak-element-ii)
+
+/*
+Topics and Learnings: Binary Search
+
+Approach:
+Used Binary Search to solve this
+
+Complexiy Analysis:
+TC: O(M * logN)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
 	vector<int> findPeakGrid(vector<vector<int>>& mat) {
@@ -24,3 +41,63 @@ public:
 		return { -1, -1 };
 	}
 };
+
+int main(int argc, const char** argv) {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n, m;
+		cin >> n >> m;
+
+		vector<vector<int>> matrix;
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			for (int i = 0; i < m; i++) {
+				int input;
+				cin >> input;
+				arr.push_back(input);
+			}
+			matrix.push_back(arr);
+			arr.clear();
+		}
+
+		Solution sol;
+
+		vector<int> res = sol.findPeakGrid(matrix);
+
+		for (auto i : res) {
+			cout << i << " ";
+		}
+
+		cout << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input :
+3
+
+3 3
+10 20 15
+21 30 14
+7 16 32
+
+2 2
+10 7
+11 17
+
+3 3
+1 2 3
+4 5 6
+7 8 9
+
+Test Output : 1
+1 1
+1 1
+2 2
+
+*/
