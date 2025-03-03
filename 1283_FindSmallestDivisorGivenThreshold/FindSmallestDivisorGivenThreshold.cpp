1e19fce64a36fa6daf5353ab2d12c7d5da7ec076
jsambhav5 <jsambhav5@gmail.com>
2025-03-03 07:51:57 +0530

    Find the Smallest Divisor Given a Threshold

--- 1283_FindSmallestDivisorGivenThreshold/FindSmallestDivisorGivenThreshold.cpp
+++ 1283_FindSmallestDivisorGivenThreshold/FindSmallestDivisorGivenThreshold.cpp
@@ -1,6 +1,24 @@
+// LeetCode : 1283 - Find the Smallest Divisor Given a Threshold (https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/)
+// TUF+ : Find the smallest divisor (https://takeuforward.org/plus/dsa/binary-search/on-answers/find-the-smallest-divisorr)
+
+/*
+Topics and Learnings: Binary Search
+
+Approach:Binary Search On Answers
+Instead of checking for every divisor from 1 to max of array, we can just check for (min + max) / 2
+where min starts with 1 and max starts with bananas in biggest pile
+if possible solution exists, then ans is in left half else ans is in right half
+
+TC: O(log(max)*N)
+SC: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 private:
-	bool possible(vector<int>& nums, int threshold, int div) {
+	bool possible(vector<int>& nums, int limit, int div) {
 		int sum = 0;
 		int n = nums.size();
 
@@ -10,11 +28,11 @@ private:
 			if (nums[i] % div != 0) sum++;
 		}
 
-		return sum <= threshold;
+		return sum <= limit;
 	}
 
 public:
-	int smallestDivisor(vector<int>& nums, int threshold) {
+	int smallestDivisor(vector<int>& nums, int limit) {
 		int low = 1, high = INT_MIN;
 		int ans = -1;
 		for (int i : nums)
@@ -23,7 +41,7 @@ public:
 		while (low <= high) {
 			int mid = (low + high) / 2;
 
-			if (possible(nums, threshold, mid)) {
+			if (possible(nums, limit, mid)) {
 				ans = mid;
 				high = mid - 1;
 			}
@@ -33,3 +51,55 @@ public:
 		return ans;
 	}
 };
+
+int main(int argc, const char** argv) {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int n;
+		cin >> n;
+
+		vector<int> arr;
+
+		for (int i = 0; i < n; i++) {
+			int input;
+			cin >> input;
+			arr.push_back(input);
+		}
+
+		int limit;
+		cin >> limit;
+
+		Solution sol;
+
+		int ans = sol.smallestDivisor(arr, limit);
+
+		cout << ans << endl;
+	}
+
+	return 0;
+}
+
+/*
+Test Input :
+3
+
+5
+1 2 3 4 5
+8
+
+4
+8 4 2 3
+10
+
+4
+8 4 2 3
+4
+
+Test Output : 1
+3
+2
+8
+
+*/
