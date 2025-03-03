e0a96d42ed301718d864b868d1330a1d2e21f69d
jsambhav5 <jsambhav5@gmail.com>
2025-03-03 07:52:57 +0530

    Find the Smallest Divisor Given a Threshold

--- 1283_FindSmallestDivisorGivenThreshold/FindSmallestDivisorGivenThreshold.cpp
+++ 1283_FindSmallestDivisorGivenThreshold/FindSmallestDivisorGivenThreshold.cpp
@@ -9,6 +9,7 @@ Instead of checking for every divisor from 1 to max of array, we can just check 
 where min starts with 1 and max starts with bananas in biggest pile
 if possible solution exists, then ans is in left half else ans is in right half
 
+Complexiy Analysis:
 TC: O(log(max)*N)
 SC: O(1)
 */
